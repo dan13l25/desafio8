c156dd3d73c0fc2eeaa59aef8c68282924333c4b
Daniel Alemán <dan13laleman14@gmail>
2024-05-07 18:01:50 -0300

    cartRouter hecho

--- routes/cartRouter.js
+++ routes/cartRouter.js
@@ -4,39 +4,10 @@ import CartController from "../dao/controllers/cartController.js";
 const cartRouter = Router();
 const cartController = new CartController();
 
-cartRouter.post("/", async (req, res) => {
-    try {
-        const newCart = await cartController.createCart();
-        res.json(newCart);
-    } catch (error) {
-        console.error("Error al obtener el carrito:", error.message);
-        res.status(500).send("Error interno del servidor");
-    }
-});
+cartRouter.post("/", cartController.createCart)
 
-cartRouter.get("/:cid", async (req, res) => {
-    const { cid } = req.params;
-    try {
-        const cart = await cartController.getCartById(cid); 
-        if (!cart) {
-            return res.status(404).send("Carrito no encontrado");
-        }
-        res.json(cart);
-    } catch (error) {
-        console.error("Error al obtener carrito:", error.message);
-        res.status(500).send("Error interno del servidor");
-    }
-});
+cartRouter.get("/:cartId", cartController.getCartById);  
 
-cartRouter.post("/:cid/product/:pid", async (req, res) => {
-    const { cid, pid } = req.params;
-    try {
-        await cartController.addProduct(cid, pid);
-        res.send("Producto agregado al carrito");
-    } catch (error) {
-        console.error("Error al agregar producto al carrito:", error.message);
-        res.status(500).send("Error interno del servidor");
-    }
-});
+cartRouter.post("/:cartId/product/:productId", cartController.addProduct)
 
 export { cartRouter };
